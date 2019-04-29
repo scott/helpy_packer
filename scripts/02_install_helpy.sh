7e8ee4b4708cf74082452b22e97a89eea6898137
scott <scott.miller.utah@gmail.com>
2019-04-29 10:16:12 -0600

    Ignore rvm warning

--- scripts/02_install_helpy.sh
+++ scripts/02_install_helpy.sh
@@ -4,6 +4,11 @@
 su - deploy -c "source /home/deploy/.rvm/scripts/rvm"
 su - deploy -c "git clone --single-branch --branch $HELPY_BRANCH https://github.com/helpyio/helpy.git /home/deploy/helpy"
 
+# Ignore warnings from RVM and ruby version
+su - deploy -c "cd /home/deploy/helpy && rvm rvmrc warning ignore /home/deploy/helpy/Gemfile"
+su - deploy -c "cd /home/deploy/helpy && rvm rvmrc warning ignore allGemfiles"
+sed -i "s/'>= 2.2', '< 3.0'/'$RUBY'/g" /home/deploy/helpy/Gemfile
+
 su - deploy -c "rm /home/deploy/helpy/config/database.yml"
 su - deploy -c "cp /tmp/database.yml /home/deploy/helpy/config/database.yml"
 su - deploy -c "cp /tmp/seeds.rb /home/deploy/helpy/db/seeds.rb"
@@ -24,7 +29,3 @@ chmod +x /var/lib/cloud/scripts/per-instance/01-firstrun.sh
 # Copy NGINX vhost
 rm /etc/nginx/sites-enabled/default
 cp /tmp/default /etc/nginx/sites-enabled/default
-
-# Copy upgrade script
-# cp /tmp/upgrade.sh /home/deploy/upgrade.sh
-# chmod +x /home/deploy/upgrade.sh
